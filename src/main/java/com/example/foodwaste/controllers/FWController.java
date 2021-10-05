934056e80ca8b9c45d40bbb05322c481f2038959
Zerkia <lordzerkia@gmail.com>
2021-10-05 12:06:14 +0200

    files by jonathan

--- src/main/java/com/example/foodwaste/controllers/FWController.java
+++ src/main/java/com/example/foodwaste/controllers/FWController.java
@@ -1,4 +1,13 @@
 package com.example.foodwaste.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class FWController {
+
+  @GetMapping("/")
+  public String index(){
+    return "index";
+  }
 }
