b6d146c41d16b5a61351a06027488ac50afc2408
Zerkia <lordzerkia@gmail.com>
2021-10-05 11:30:33 +0200

    initial commit

---
+++ src/main/java/com/example/foodwaste/FoodwasteApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodwaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodwasteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FoodwasteApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/example/foodwaste/controllers/FWController.java
@@ -0,0 +1,4 @@
+package com.example.foodwaste.controllers;
+
+public class FWController {
+}
